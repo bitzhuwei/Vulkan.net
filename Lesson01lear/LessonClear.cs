862cd8aaef15fa2bd9cfa250d0e2e99b4b3302ff
bitzhuwei <bitzhuwei.cnblogs@qq.com>
2019-08-26 00:03:00 +0800

    LessonClear

---
+++ Lesson01lear/LessonClear.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson01lear {
+    class LessonClear {
+        public void Init() {
+
+        }
+
+        public void Render() {
+
+        }
+    }
+}
