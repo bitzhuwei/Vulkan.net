5d9ea81385d3d56784165fa4fd72f19593b53c26
bitzhuwei <bitzhuwei.cnblogs@qq.com>
2019-08-31 08:51:45 +0800

    helper

--- Vulkan/_Helper.cs
+++ Vulkan/_Helper.cs
@@ -167,4 +167,34 @@ namespace Vulkan {
         //    return bytes;
         //}
     }
+
+    public static class ResultHelper {
+        public static VkResult Check(this VkResult result) {
+            if (result != VkResult.Success) { throw new ResultException(result); }
+
+            return result;
+        }
+    }
+
+    public class ResultException : Exception {
+        internal VkResult result;
+
+        public VkResult Result {
+            get { return result; }
+        }
+
+        internal ResultException(VkResult res) {
+            result = res;
+        }
+    }
+
+    public class Version {
+        public static uint Make(uint major, uint minor, uint patch) {
+            return (major << 22) | (minor << 12) | patch;
+        }
+
+        public static string ToString(uint version) {
+            return string.Format("{0}.{1}.{2}", version >> 22, (version >> 12) & 0x3ff, version & 0xfff);
+        }
+    }
 }
