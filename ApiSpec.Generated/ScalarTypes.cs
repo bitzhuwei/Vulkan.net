996d2ad62cf87abf74adb5441d45f11fc81865d2
bitzhuwei <bitzhuwei.cnblogs@qq.com>
2019-09-12 21:55:18 +0800

    VkBool32

--- ApiSpec.Generated/ScalarTypes.cs
+++ ApiSpec.Generated/ScalarTypes.cs
@@ -11,6 +11,41 @@ namespace ApiSpec.Generated {
         public override string ToString() {
             return $"{nameof(VkBool32)}: {this.value != 0}";
         }
+
+        public static bool operator !(VkBool32 v) {
+            return (v.value == 0);
+        }
+
+        // == !=
+        public static bool operator ==(VkBool32 left, VkBool32 right) {
+            return left.value == right.value;
+        }
+        public static bool operator !=(VkBool32 left, VkBool32 right) {
+            return left.value != right.value;
+        }
+
+        public static bool operator ==(VkBool32 left, bool right) {
+            return (left.value != 0) == right;
+        }
+        public static bool operator !=(VkBool32 left, bool right) {
+            return (left.value != 0) != right;
+        }
+
+        public static bool operator ==(bool left, VkBool32 right) {
+            return left == (right.value != 0);
+        }
+        public static bool operator !=(bool left, VkBool32 right) {
+            return left != (right.value != 0);
+        }
+
+        public override bool Equals(object obj) {
+            return this.value.Equals(obj);
+        }
+
+        public override int GetHashCode() {
+            return this.value.GetHashCode();
+        }
+
     }
 
     public unsafe struct VkDeviceAddress {
