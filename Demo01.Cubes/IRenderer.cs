ef04824b0223a6be8a9ebd0472002d7805708146
bitzhuwei <bitzhuwei.cnblogs@qq.com>
2019-09-17 23:27:26 +0800

    cubes, transformation, camera, etc.

---
+++ Demo01.Cubes/IRenderer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo01.Cubes {
+    /// <summary>
+    /// 
+    /// </summary>
+    public interface IRenderer {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="canvas"></param>
+        void Init(IntPtr canvas);
+
+        /// <summary>
+        /// 
+        /// </summary>
+        void Render();
+    }
+}
---
+++ Demo01.Cubes/VkWinCanvas.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace Demo01.Cubes {
+    public partial class VkWinCanvas : UserControl {
+
+        IRenderer renderer;
+
+        protected readonly bool designMode;
+
+        private Timer timer = new Timer();
+
+        public VkWinCanvas() : this(null) { }
+
+        public VkWinCanvas(IRenderer renderer) {
+            InitializeComponent();
+
+            this.renderer = renderer;
+
+            this.designMode = this.DesignMode || System.ComponentModel.LicenseManager.UsageMode == System.ComponentModel.LicenseUsageMode.Designtime;
+            if (this.designMode) {
+                this.BackColor = Color.Red;
+            }
+        }
+
+        protected override void OnLoad(EventArgs e) {
+            base.OnLoad(e);
+
+            if (!this.designMode) {
+                this.renderer.Init(this.Handle);
+                this.timer.Interval = 50;
+                this.timer.Tick += Timer_Tick;
+                this.timer.Enabled = true;
+            }
+        }
+
+        private void Timer_Tick(object sender, EventArgs e) {
+            this.Invalidate();
+        }
+
+        protected override void OnPaintBackground(PaintEventArgs e) {
+            if (this.designMode) {
+                base.OnPaintBackground(e);
+            }
+            else {
+                IRenderer renderer = this.renderer;
+                if (renderer != null) {
+                    renderer.Render();
+                }
+                else {
+                    base.OnPaintBackground(e);
+                }
+            }
+        }
+    }
+}
