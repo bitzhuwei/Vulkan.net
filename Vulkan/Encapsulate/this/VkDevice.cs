297933c3782d1c7f0403c5928f8d5daf7ed9f8e5
bitzhuwei <bitzhuwei.cnblogs@qq.com>
2019-10-16 22:08:13 +0800

    update

--- Vulkan/Encapsulate/this/VkDevice.cs
+++ Vulkan/Encapsulate/this/VkDevice.cs
@@ -3,33 +3,31 @@ using System.Collections.Generic;
 using System.Text;
 
 namespace Vulkan {
-    public unsafe static class VkDeviceHelper {
+    public unsafe partial struct VkDevice {
         // Command: 208
         /// <summary>vkGetDeviceQueue - Get a queue handle from a device
         /// </summary>
-        /// <param name="device"> device is the logical device that owns the queue.</param>
         /// <param name="queueFamilyIndex"> queueFamilyIndex is the index of the queue family to which the
         /// queue belongs.</param>
         /// <param name="queueIndex"> queueIndex is the index within this queue family of the queue to
         /// retrieve.</param>
-        public static VkQueue GetQueue(this VkDevice device, UInt32 queueFamilyIndex, UInt32 queueIndex) {
+        public VkQueue GetQueue(UInt32 queueFamilyIndex, UInt32 queueIndex) {
             VkQueue queue;
-            vkAPI.vkGetDeviceQueue(device, queueFamilyIndex, queueIndex, &queue);
+            vkAPI.vkGetDeviceQueue(this, queueFamilyIndex, queueIndex, &queue);
             return queue;
         }
 
         // Command: 285
         /// <summary>vkGetSwapchainImagesKHR - Obtain the array of presentable images associated with a swapchain
         /// </summary>
-        /// <param name="device"> device is the device associated with swapchain.</param>
         /// <param name="swapchain"> swapchain is the swapchain to query.</param>
-        public static VkImage[] GetSwapchainImages(this VkDevice device, VkSwapchainKHR swapchain) {
+        public VkImage[] GetSwapchainImages(VkSwapchainKHR swapchain) {
             UInt32 count;
-            vkAPI.vkGetSwapchainImagesKHR(device, swapchain, &count, null).Check();
+            vkAPI.vkGetSwapchainImagesKHR(this, swapchain, &count, null).Check();
             var vkImages = new VkImage[count];
             if (count > 0) {
                 fixed (VkImage* pointer = vkImages) {
-                    vkAPI.vkGetSwapchainImagesKHR(device, swapchain, &count, pointer).Check();
+                    vkAPI.vkGetSwapchainImagesKHR(this, swapchain, &count, pointer).Check();
                 }
             }
 
--- Vulkan/Encapsulate/this/VkSwapchainKHR.cs
+++ Vulkan/Encapsulate/this/VkSwapchainKHR.cs
@@ -3,20 +3,18 @@ using System.Collections.Generic;
 using System.Text;
 
 namespace Vulkan {
-    public unsafe static class VkSwapchainKHRHelper {
-
+    public unsafe partial struct VkSwapchainKHR {
         // Command: 285
         /// <summary>vkGetSwapchainImagesKHR - Obtain the array of presentable images associated with a swapchain
         /// </summary>
-        /// <param name="swapchain"> swapchain is the swapchain to query.</param>
         /// <param name="device"> device is the device associated with swapchain.</param>
-        public static VkImage[] GetImages(this VkSwapchainKHR swapchain, VkDevice device) {
+        public VkImage[] GetImages(VkDevice device) {
             UInt32 count;
-            vkAPI.vkGetSwapchainImagesKHR(device, swapchain, &count, null).Check();
+            vkAPI.vkGetSwapchainImagesKHR(device, this, &count, null).Check();
             var vkImages = new VkImage[count];
             if (count > 0) {
                 fixed (VkImage* pointer = vkImages) {
-                    vkAPI.vkGetSwapchainImagesKHR(device, swapchain, &count, pointer).Check();
+                    vkAPI.vkGetSwapchainImagesKHR(device, this, &count, pointer).Check();
                 }
             }
 
